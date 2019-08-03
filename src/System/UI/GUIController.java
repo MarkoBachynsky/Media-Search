37c17267ad184b964b0d0805ab85eae37111231c
Marko <Bachynsky.M956243@stu.SanJac.edu>
2019-08-03 11:52:25 -0500

    Refactor 2

---
+++ src/System/UI/GUIController.java
@@ -0,0 +1,18 @@
+package System.UI;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * The controller class for GUI.FXML.
+ * 
+ * @author Marko S. Bachynsky
+ * @since 1.0
+ */
+
+public class GUIController
+{
+	@FXML
+	public BorderPane Root;
+
+}
