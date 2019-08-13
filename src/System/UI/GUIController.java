f7f8be11a4390163d480cf2eaa46052d2f58f031
Marko <Bachynsky.M956243@stu.SanJac.edu>
2019-08-13 07:48:09 -0500

    MS-2 #time 10m
    Simple population figured out, blocked by MS-3 now.

--- src/System/UI/GUIController.java
+++ src/System/UI/GUIController.java
@@ -1,6 +1,11 @@
 package System.UI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -14,5 +19,17 @@ public class GUIController
 {
 	@FXML
 	public BorderPane Root;
+	@FXML
+	public ListView<String> ListViewMedia;
+	@FXML
+	public Button ButtonAddMedia;
+
+
+	public void OnActionButtonAddMedia(ActionEvent event)
+	{
+		ObservableList<String> names = FXCollections.observableArrayList( "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+		ListViewMedia.setItems(names);
+	}
+
 
 }
